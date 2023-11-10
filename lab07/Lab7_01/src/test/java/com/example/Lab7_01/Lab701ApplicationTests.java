da176beaf8854f56e34290fef83527df440891e9
Trương Thái Đan Huy <117837212+CookiesDan220603@users.noreply.github.com>
2023-11-10 22:48:04 +0700

    Add files via upload

---
+++ lab07/Lab7_01/src/test/java/com/example/Lab7_01/Lab701ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab7_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab701ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
